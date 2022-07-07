5e8c8b8066ce551f73d505269637cb1aa7f93743
GAURAV CHOUDHARY <46090043+GauravChoudhay@users.noreply.github.com>
2022-07-07 07:06:29 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (18.68%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -21,10 +21,6 @@ class Solution {
             return null;
         }
 
-        if(root.left == null && root.right == null && root.val == target){
-            return null;
-        }
-
         root.left = removeLeafNodes(root.left,target);
         root.right = removeLeafNodes(root.right,target);
         
