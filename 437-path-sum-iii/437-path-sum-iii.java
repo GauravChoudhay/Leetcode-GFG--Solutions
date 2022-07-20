c9b95619b49bb2fdc55b2da2ea7545e0ab76d1d2
GAURAV CHOUDHARY <46090043+GauravChoudhay@users.noreply.github.com>
2022-07-21 05:28:38 +0530

    Time: 24 ms (37.09%), Space: 43.3 MB (79.27%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -17,33 +17,34 @@ class Solution {
     
     int count;
     
-    public void pathSumHelper(TreeNode root, long tar){       
+    public void nodeToAnyNodePathCount(TreeNode root, long tar){       
                 
         if(root==null) return ;
         
         if(tar==root.val) count++;     
                 
         
-        pathSumHelper(root.left,tar-root.val);
-        pathSumHelper(root.right,tar-root.val);
+        nodeToAnyNodePathCount(root.left,tar-root.val);
+        nodeToAnyNodePathCount(root.right,tar-root.val);
         return;
+        
+    }
+    public void pathSumHelper(TreeNode root, int targetSum) {
+        
+        if(root==null) return ;        
+        
+        nodeToAnyNodePathCount(root,(long)targetSum);
+        
+        pathSumHelper(root.left,targetSum);
+        pathSumHelper(root.right,targetSum);
         
     }
     
     public int pathSum(TreeNode root, int targetSum) {
         
-        
-        if(root==null) return 0;        
-        
-        count = 0;    
-        pathSumHelper(root,(long)targetSum);
-        int Ocount = count;
         count = 0;
-        
-        Ocount+= pathSum(root.left,targetSum);
-        Ocount+= pathSum(root.right,targetSum);
-        
-        return Ocount;
+        pathSumHelper(root,targetSum);
+        return count;
     }
 }
 
